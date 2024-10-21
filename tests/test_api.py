f5192aa8282ab3bb204516bae853dbec8cf8c779
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-10-21 13:19:04 +0200

    Initial commit

---
+++ tests/test_api.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from fastapi_hexa.main import app
+
+test_client = TestClient(app)
+
+
+def test_get_index() -> None:
+    response = test_client.get("/")
+
+    assert response.is_success
