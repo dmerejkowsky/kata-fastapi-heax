4f525a1ff54a951af9676f9f17c6d7f2e3a4cb87
Eric Dasse <christ.gououi@gmail.com>
2024-10-21 19:05:21 +0200

    Add response data classes with pydantic

--- fastapi_hexa/main.py
+++ fastapi_hexa/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +7,18 @@ app = FastAPI()
 @app.get("/")
 def index() -> str:
     return "hello"
+
+
+class Seat(BaseModel):
+    booking_reference: str
+    seat_number: str
+    coach: str
+
+
+class TrainData(BaseModel):
+    seats: dict[str, Seat]
+
+
+@app.get("/data_for_train/{train_id}")
+def get_data_for_train(train_id: int) -> TrainData:
+    return TrainData(seats={})
